a296c9463fa52e387343d4aa30b7c2a2f447d81d
Anna Vissart <anna.vissart@ritense.com>
2024-10-23 19:59:24 +0200

    * separate TaakObjectV2 domain object from TaakObject

--- zgw/portaaltaak/src/main/kotlin/com/ritense/portaaltaak/TaakObject.kt
+++ zgw/portaaltaak/src/main/kotlin/com/ritense/portaaltaak/TaakObject.kt
@@ -16,12 +16,10 @@
 
 package com.ritense.portaaltaak
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonValue
 import java.net.URI
 import java.time.LocalDateTime
-import java.util.UUID
 
 class TaakObject(
     val identificatie: TaakIdentificatie,
@@ -38,53 +36,8 @@ class TaakObject(
     val verzondenData: Map<String, Any> = mapOf(),
 )
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class TaakObjectV2(
-    val titel: String,
-    var status: TaakStatus,
-    val soort: TaakSoort,
-    val verloopdatum: LocalDateTime?,
-    val identificatie: TaakIdentificatie,
-    val koppeling: TaakKoppeling,
-    val url: TaakUrl?,
-    val portaalformulier: TaakForm?,
-    val ogonebetaling: OgoneBetaling?,
-    @JsonProperty("verwerker_taak_id") val verwerkerTaakId: UUID,
-    val eigenaar: String,
-)
-
-enum class TaakSoort(
-    @JsonValue val value: String,
-) {
-    URL("url"),
-    PORTAALFORMULIER("portaalformulier"),
-    OGONEBETALING("ogonebetaling"),
-    ;
-
-    override fun toString(): String = value
-}
-
-enum class TaakKoppelingRegistratie(
-    @JsonValue val value: String,
-) {
-    ZAAK("zaak"),
-    PRODUCT("product"),
-}
-
-data class TaakKoppeling(
-    val registratie: TaakKoppelingRegistratie,
-    val uuid: UUID?,
-)
-
 data class TaakUrl(
     val uri: String,
-)
-
-
-data class OgoneBetaling(
-    val bedrag: Double,
-    val betaalkenmerk: String,
-    val pspid: String,
 )
 
 class TaakIdentificatie(
---
+++ zgw/portaaltaak/src/main/kotlin/com/ritense/portaaltaak/TaakObjectV2.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.portaaltaak
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
+import java.time.LocalDateTime
+import java.util.UUID
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class TaakObjectV2(
+    val titel: String,
+    var status: TaakStatus,
+    val soort: TaakSoort,
+    val verloopdatum: LocalDateTime? = null,
+    val identificatie: TaakIdentificatie,
+    val koppeling: TaakKoppeling,
+    val url: TaakUrl? = null,
+    val portaalformulier: PortaalFormulier? = null,
+    val ogonebetaling: OgoneBetaling? = null,
+    @JsonProperty("verwerker_taak_id") val verwerkerTaakId: UUID,
+    val eigenaar: String,
+) {
+    enum class TaakSoort(
+        @JsonValue val value: String,
+    ) {
+        URL("url"),
+        PORTAALFORMULIER("portaalformulier"),
+        OGONEBETALING("ogonebetaling"),
+        ;
+
+        override fun toString(): String = value
+    }
+
+    enum class TaakStatus(@JsonValue val value: String) {
+        OPEN("open"),
+        AFGEROND("afgerond"),
+        VERWERKT("verwerkt"),
+        GESLOTEN("gesloten"),
+        ;
+
+        override fun toString(): String = value
+    }
+
+    enum class TaakFormulierSoort(@JsonValue val value: String) {
+        ID("id"),
+        URL("url"),
+        ;
+
+        override fun toString(): String = value
+    }
+
+    enum class TaakKoppelingRegistratie(
+        @JsonValue val value: String,
+    ) {
+        ZAAK("zaak"),
+        PRODUCT("product"),
+    }
+
+    data class TaakKoppeling(
+        val registratie: TaakKoppelingRegistratie,
+        val uuid: UUID?,
+    )
+
+    data class OgoneBetaling(
+        val bedrag: Double,
+        val betaalkenmerk: String,
+        val pspid: String,
+    )
+
+    data class TaakIdentificatie(
+        val type: String,
+        val value: String
+    )
+
+    data class PortaalFormulier(
+        val type: TaakFormType,
+        val data: Map<String, Any>? = emptyMap(),
+        @JsonProperty("verzonden_data")
+        var verzondenData: Map<String, Any>? = null,
+    )
+
+    data class TaakFormulier(
+        val soort: TaakFormulierSoort,
+        val value: String
+    )
+}
